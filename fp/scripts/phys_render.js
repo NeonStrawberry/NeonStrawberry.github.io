9a0a641e8c3beed00eb8c022d3e4388d7e508311
NeonStrawberry <96705137+NeonStrawberry@users.noreply.github.com>
2024-08-23 17:14:08 -0300

    Update game

--- fp/scripts/phys_render.js
+++ fp/scripts/phys_render.js
@@ -1,6 +1,6 @@
 // Copyright (C) 2024 NeonStrawberry
 
-const health_max = 5;
+// const health_max = 5;
 const iframes_max = 50; // 40TPS = 3/4 second iframes
 const power_cooldown = 8; // Ticks to wait between power increases
 const power_penalty = 60; // Ticks to wait before beginning to charge power after running out
@@ -8,18 +8,21 @@ const score_penalty = 50; // Score to lose when hit
 const score_reward = 100; // Score to gain when an enemy is killed
 const p_width = 16; // Size of the player in pixels
 const p_speed = 5;
-const weapon_cooldown = 10; // Ticks to wait between weapon shots
-const p_projectile_speed = 7;
+const weapon_cooldown = 5; // Ticks to wait between weapon shots
+const p_projectile_speed = 30;
 const p_projectile_width = 10;
-const p_wpncost = 5; // Power consumed by weapon
+const p_wpncost = 2; // Power consumed by weapon
 const max_power = 50;
 const max_enemy_cooldown = 160; // Ticks to wait between an enemy's death and a new one spawning
-const enemy_max_health = 2;
+const enemy_max_health = 10;
+const projectile_speed = 2;
+const enemy_count = 1;
+const bullet_lifetime = 800;
 
 let mkeys = []; // Keys currently held down
 let p_pos = [450, 337];
 let p_power = 1;
-let p_health = health_max;
+let p_health = 1;
 let p_iframes = 0; // The iframes available
 let p_score = 0; // Increase by 1 every tick, lose 50 when hit, increase by 100 when killing an enemy
 let p_wpncool = 0; // Weapon cooldown
@@ -28,6 +31,8 @@ let p_pwrcool = 0; // Power increase cooldown
 let p_projectiles = []; // Player's bullets
 let projectiles = []; // Enemies' bullets
 let enemies = [];
+
+let waves = 0; // Number of waves so far
 
 let enemy_cooldown = max_enemy_cooldown;
 
@@ -63,8 +68,8 @@ function collides_with_player(pos, size) {
 
 function collides_with_enemy(pos, size) {
     for (let i = 0; i < enemies.length; i++) {
-        if (Math.abs(pos[0] - enemies[i].pos[0]) <= (size / 2 + p_width / 2))
-            if (Math.abs(pos[1] - enemies[i].pos[1]) <= (size / 2 + p_width / 2))
+        if (Math.abs(pos[0] - enemies[i].pos[0]) <= (size / 2 + enemies[i].width / 2))
+            if (Math.abs(pos[1] - enemies[i].pos[1]) <= (size / 2 + enemies[i].width / 2))
                 return i;
     }
 
@@ -76,6 +81,7 @@ class Projectile {
     constructor(pos, vel) {
         this.pos = [pos[0], pos[1]];
         this.vel = [vel[0], vel[1]];
+        this.lifetime = bullet_lifetime;
     }
 
     // Return -1 to delete
@@ -115,6 +121,10 @@ class BasicEnemyBullet extends Projectile {
             p_hurt();
             return -1;
         }
+
+        this.lifetime--;
+        if (this.lifetime <= 0)
+            return -1;
     }
 
     draw(context) {
@@ -130,7 +140,7 @@ class Enemy {
         this.pos = [450, 80];
         this.width = 20;
         this.speed = 1;
-        this.max_bcool = 40;
+        this.max_bcool = 30;
 
         this.aim_time = 0; // Ticks to move in a direction
         this.aim_direction = 0; // Direction to move in. 0 = left, 1 = right
@@ -138,7 +148,7 @@ class Enemy {
     }
 
     ai() {
-        if (this.aim_time == 0) {
+        if (this.aim_time == 0 || this.pos[0] < this.width / 2 || this.pos[0] > 900 - this.width / 2) {
             this.aim_time = 20 + Math.floor(Math.random() * 100);
             this.aim_direction = 1 - this.aim_direction;
         }
@@ -159,6 +169,37 @@ class Enemy {
     }
 }
 
+class Boss extends Enemy {
+    constructor() {
+        super();
+
+        this.health = 100;
+        this.pos = [450, 80];
+        this.width = 40;
+        this.speed = 4;
+        this.max_bcool = 40;
+
+        this.bullet_cooldown = this.max_bcool;
+        this.aim_direction = 0;
+    }
+
+    ai() {
+        if (this.aim_time == 0 || this.pos[0] < this.width / 2 || this.pos[0] > 900 - this.width / 2) {
+            this.aim_time = 20 + Math.floor(Math.random() * 100);
+            this.aim_direction = 1 - this.aim_direction;
+        }
+
+        this.aim_time--;
+        this.bullet_cooldown--;
+
+        this.pos[0] += (this.aim_direction - 0.5) * 2 * this.speed;
+        if (this.bullet_cooldown <= 0) {
+            bullet_star(this.pos, 50);
+            this.bullet_cooldown = this.max_bcool;
+        }
+    }
+}
+
 function game_over() {
     alert("u are ded");
     reset();
@@ -168,7 +209,7 @@ function game_over() {
 function reset() {
     p_pos = [450, 337];
     p_power = 1;
-    p_health = health_max;
+    p_health = 1;
     p_iframes = 0;
     p_score = 0;
     p_wpncool = 0;
@@ -179,6 +220,7 @@ function reset() {
     enemies = [];
 
     enemy_cooldown = max_enemy_cooldown;
+    waves = 0;
 }
 
 /* Check if an enemy should spawn */
@@ -187,14 +229,34 @@ function check_enemy_spawn() {
         if (enemy_cooldown > 0)
             enemy_cooldown--;
         else {
+            waves++;
             enemy_cooldown = max_enemy_cooldown;
             return 1;
         }
     }
 }
 
+function spawn_boss() {
+    enemies.push(new Boss());
+}
+
 function spawn_enemy() {
-    enemies.push(new Enemy());
+    if ((waves) % 3 != 0) {
+        for (let i = 0; i < enemy_count; i++) {
+            enemies.push(new Enemy());
+        }
+    } else {
+        spawn_boss();
+    }
+}
+
+function bullet_star(pos, num) {
+    for (let i = 0; i < num; i++) {
+        let vx = Math.sin(i / num * 6.28319) * projectile_speed / 2;
+        let vy = Math.cos(i / num * 6.28319) * projectile_speed / 2;
+
+        projectiles.push(new BasicEnemyBullet(pos, [vx, vy]));
+    }
 }
 
 /* Shoot a projectile from the player */
@@ -204,7 +266,7 @@ function player_shoot() {
 }
 
 function enemy_shoot(en) {
-    let vel = [0, p_projectile_speed];
+    let vel = [0, projectile_speed];
     projectiles.push(new BasicEnemyBullet(en.pos, vel));
 }
 
@@ -231,6 +293,8 @@ function enemy_updates() {
     for (let i = 0; i < enemies.length; i++) {
         enemies[i].ai();
         if (enemies[i].health <= 0) {
+            p_health++;
+            p_score += score_reward;
             enemies.splice(i, 1);
             i--;
         }    
